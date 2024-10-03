037d9e14694c90742333a6c3658de9911bb6fc77
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-10-04 01:04:21 +0900

    Fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+mkdir build
+pushd build
+
 export XDG_DATA_DIRS="${XDG_DATA_DIRS}:${PREFIX}/share:${BUILD_PREFIX}/share"
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${PREFIX}/lib/pkgconfig:${BUILD_PREFIX}/lib/pkgconfig"
 
